9c7426778d00c85447ae4da66b81e89f3308a964
Kelly Hondula <khondula@rstudio03.research.sesync.org>
2020-02-04 12:49:23 -0500

    updates

--- bigmaps.R
+++ bigmaps.R
@@ -7,25 +7,30 @@ data_dir <- "/nfs/public-data/NEON_workshop_data/bigmaps"
 
 site_centroids <- read_csv("data/site_centroids.csv")
 
-neon_sites_sf <- readr::read_csv("data/field-sitesNEON.csv") %>%
-  sf::st_as_sf(neon_sites, 
-               coords = c('Longitude', 'Latitude'),
-               crs = 4326)
+neon_sites <- readr::read_csv("data/field-sitesNEON.csv")
 
-save_bigmap <- function(site_no){
+neon_sites_sf <- sf::st_as_sf(neon_sites, 
+                              coords = c('Longitude', 'Latitude'),
+                              crs = 4326)
+
+save_bigmap <- function(site_no, my_vwidth = 12000, my_vheight = 8000){
   
   mp <- leaflet(neon_sites_sf) %>%
     setView(lng = site_centroids$X[site_no], 
             lat = site_centroids$Y[site_no], 
             zoom = 16) %>%
     addProviderTiles(providers$Esri.WorldImagery, group = "Esri World Imagery") %>%
-    addPolygons(data = all_aops_sf, fillOpacity = 0, opacity = 1, color = "red") 
+    addPolygons(data = all_aops_sf, weight = 10, 
+                fillOpacity = 0, opacity = 1, color = "yellow") 
   
   
   mapshot(mp, 
           file = glue::glue("{data_dir}/map_site-{site_no}.pdf"), 
-          vwidth = 12000, vheight = 8000)
+          vwidth = my_vwidth, vheight = my_vheight)
 }
 
-save_bigmap(site_no = 1)
-purrr::walk(11:70, ~save_bigmap(.x))
+save_bigmap(site_no = 17, my_vwidth = 16000)
+save_bigmap(site_no = 19, my_vheight = 9000)
+save_bigmap(site_no = 20, my_vheight = 9000)
+
+# purrr::walk(11:70, ~save_bigmap(.x))
